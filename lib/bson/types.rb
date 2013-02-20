cc834d1df4b52a27b2238cec12b1a2c629a25c1d
Durran Jordan <durran@gmail.com>
2013-02-20 21:27:35 +0100

    Adding type mappings

--- lib/bson/types.rb
+++ lib/bson/types.rb
@@ -10,6 +10,7 @@ module BSON
   #
   # @since 2.0.0
   module Types
+    extend self
 
     # A string is type 0x02 in the BSON spec.
     #
@@ -20,5 +21,28 @@ module BSON
     #
     # @since 2.0.0
     MIN_KEY = 255.chr.freeze
+
+    # A Mapping of all the BSON types to their corresponding Ruby classes.
+    #
+    # @since 2.0.0
+    MAPPINGS = {
+      STRING  => String,
+      MIN_KEY => MinKey
+    }
+
+    # Get the class for the single byte identifier for the type in the BSON
+    # specification.
+    #
+    # @example Get the type for the byte.
+    #   BSON::Types.get("\x01")
+    #
+    # @return [ Class ] The corresponding Ruby class for the type.
+    #
+    # @see http://bsonspec.org/#/specification
+    #
+    # @since 2.0.0
+    def get(byte)
+      MAPPINGS.fetch(byte)
+    end
   end
 end
--- spec/bson/types_spec.rb
+++ spec/bson/types_spec.rb
@@ -2,6 +2,36 @@ require "spec_helper"
 
 describe BSON::Types do
 
+  describe ".get" do
+
+    context "when the type has a correspoding class" do
+
+      let(:klass) do
+        described_class.get(BSON::Types::MIN_KEY)
+      end
+
+      it "returns the class" do
+        expect(klass).to eq(BSON::MinKey)
+      end
+    end
+
+    context "when the type has no corresponding class" do
+
+      it "raises an error" do
+        expect {
+          described_class.get("test")
+        }.to raise_error(KeyError)
+      end
+    end
+  end
+
+  describe "::MIN_KEY" do
+
+    it "returns 0xFF" do
+      expect(BSON::Types::MIN_KEY).to eq(255.chr)
+    end
+  end
+
   describe "::STRING" do
 
     it "returns 0x02" do
