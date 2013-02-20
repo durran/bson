a01edfb94ab647b3347d8d3e4a4120a48b1bad37
Durran Jordan <durran@gmail.com>
2013-02-20 17:54:22 +0100

    Update requires with jruby example

--- lib/bson.rb
+++ lib/bson.rb
@@ -3,8 +3,20 @@ require "bson/document"
 require "bson/types"
 require "bson/version"
 
+# Determin if we are using JRuby or not.
+#
+# @example Are we running with JRuby?
+#   jruby?
+#
+# @return [ true, false ] If JRuby is our vm.
+#
+# @since 2.0.0
+def jruby?
+  RUBY_ENGINE == "jruby"
+end
+
 begin
-  require "bson/native"
+  jruby? ? require("bson/native.jar") : require("bson/native")
 rescue LoadError
   $stderr.puts("BSON is using the pure Ruby buffer implementation.")
 end
