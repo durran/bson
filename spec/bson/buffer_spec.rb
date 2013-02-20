c24a9e80ea327d294e95abb379717718d374e63b
Durran Jordan <durran@gmail.com>
2013-02-20 17:29:22 +0100

    Asser specific bytes get written

--- spec/bson/buffer_spec.rb
+++ spec/bson/buffer_spec.rb
@@ -51,7 +51,54 @@ describe BSON::Buffer do
     it_behaves_like "a fluid interface"
   end
 
-  pending "#write_cstring"
-  pending "#write_int32"
-  pending "#write_string"
+  describe "#write_cstring" do
+
+    let(:buffer) do
+      described_class.new
+    end
+
+    let!(:written) do
+      buffer.write_cstring("test")
+    end
+
+    it "writes the cstring to the buffer" do
+      expect(buffer.bytes).to eq("test\x00")
+    end
+
+    it_behaves_like "a fluid interface"
+  end
+
+  describe "#write_int32" do
+
+    let(:buffer) do
+      described_class.new
+    end
+
+    let!(:written) do
+      buffer.write_int32(4)
+    end
+
+    it "writes the integer to the buffer" do
+      expect(buffer.bytes).to eq("\x04\x00\x00\x00")
+    end
+
+    it_behaves_like "a fluid interface"
+  end
+
+  describe "#write_string" do
+
+    let(:buffer) do
+      described_class.new
+    end
+
+    let!(:written) do
+      buffer.write_string("test")
+    end
+
+    it "writes the string to the buffer" do
+      expect(buffer.bytes).to eq("\x05\x00\x00\x00test\x00")
+    end
+
+    it_behaves_like "a fluid interface"
+  end
 end
